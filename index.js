78680f8925151741b01fc4b44965e1c84dc4237e
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2020-12-04 02:09:11 -0800

    Applied function to write md file,testing first two prompt questions to create README.md

--- index.js
+++ index.js
@@ -1,79 +1,91 @@
 const inquirer = require("inquirer");
-// const fs = require("fs");
-// const md = require('./utils/generateMarkdown');
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-// const questions = [
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of the project?",
+        name: "title",
+      },
+      {  
+        type: "input",
+        message: "Please write a short description about the project.",
+        name: "description",
+      },
+      //Start with 2 questions
+    //   {
+    //     //look at different types  
+    //     type: "input",
+    //     message: "Please provide a table of contents.",
+    //     name: "tableOfContents",
+    //   },
+    //   {
+    //     type: "input",
+    //     message: "Please explain how to install the project.",
+    //     name: "installation",
+    //   },
+    //   {
+    //     type: "input",
+    //     message: "How do you use your project?",
+    //     name: "usage",
+    //   },
+    //   {
+    //     type: "input",
+    //     message: "What license does our project contain?",
+    //     name: "license",
+    //   },
+    //   {
+    //     type: "input",
+    //     message: "Who has contributed to this project?",
+    //     name: "contributing",
+    //   },
+    //   {
+    //     type: "input",
+    //     message: "Please explain how to test your project.",
+    //     name: "test",
+    //   },
+    //   {
+    //     type: "input",
+    //     message: "What is your GitHub username?",
+    //     name: "github",
+    //   },
+    //   {
+    //     type: "input",
+    //     message: "What is your email?",
+    //     name: "email",
+    //   },  
+  ];
 
+// function to write README file
+ function writeToFile(fileName, data) {
+
+    const content = generateMarkdown(data);
+     
+    fs.writeFile(fileName, content, (err) => {
+ 
+        if (err) console.log("Failed to write file");
+
+        else console.log("Wrote file.");
+    })
+ }
+
+// function to initialize program
+function init() {
+   
 inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is the title of your project?",
-      name: "title",
-    },
-    {
-      type: "input",
-      message: "Please write a short description about your project.",
-      name: "description",
-    },
-    {
-      type: "input",
-      message: "Please provide a table of contents.",
-      name: "tableOfContents",
-    },
-    {
-      type: "input",
-      message: "Please explain how to install your project.",
-      name: "installation",
-    },
-    {
-      type: "input",
-      message: "How do you use your project?",
-      name: "usage",
-    },
-    {
-      type: "input",
-      message: "What license does our project contain?",
-      name: "license",
-    },
-    {
-      type: "input",
-      message: "Who has contributed to this project?",
-      //create a list?
-      name: "contributing",
-    },
-    {
-      type: "input",
-      message: "Please explain how to test your project.",
-      name: "test",
-    },
-    {
-      type: "input",
-      message: "What is your GitHub username?",
-      name: "github",
-    },
-    {
-      type: "input",
-      message: "What is your email?",
-      name: "email",
-    },  
-])
+    .prompt(questions)
+    .then(data => {
+    
+        console.log(data);
 
-.then(response => {
-    console.log(response.title);
-})  
-;
+        data.title;
+       
+        writeToFile("UPDATED_README.md", data);
 
+ });
+}
 
-// // function to write README file
-// function writeToFile(fileName, data) {
-// }
-
-// // function to initialize program
-// function init() {
-
-// }
-
-// // function call to initialize program
-// init();
+// function call to initialize program
+init();
